f17619a1cb64385116c8b061f40248b876c709b3
Vision <vanda.vision@aol.com>
2023-02-20 11:44:14 +0330

    add model realtor and listing

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    discription =models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=timezone.now ,blank=True)
+    
+    
+    def __str__(self) -> str:
+        return self.name
