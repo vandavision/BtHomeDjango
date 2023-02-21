1b59a8ef0e48910f7b228dd5a01aadc4c61f0ef2
Vision <vanda.vision@aol.com>
2023-02-21 06:49:14 -0800

    humanize add in settings custom admin listings and custom admin realtor and add view listings

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,13 @@ from . models import Listing
 # Register your models here.
 @admin.register(Listing)
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("title", "city", "is_published", "list_date")
+    list_display = (
+        "id", "title", "is_published", 
+        "price", 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ("realtor",)
+    list_editable = ("is_published",)
+    search_fields = (
+        "title", "discription", "address", 
+        "city", "state", "zipcode", 'price')
+    list_per_page = 25
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,7 @@ from . models import Realtor
 # Register your models here.
 @admin.register(Realtor)
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ("name", "phone", "email", "is_mvp")
+    list_display = ( 'id' ,"name", "email", "hire_date")
+    list_display_links = ("id", "name")
+    search_fields = ('name',)
+    list_per_page = 25
